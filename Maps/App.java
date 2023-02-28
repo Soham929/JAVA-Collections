35f26e53df78bb6ad37e21a96be2774e811cedd4
Soham <ghoshsoham1982@gmail.com>
2023-02-28 11:25:10 +0530

    Maps code added

---
+++ Maps/App.java
@@ -0,0 +1,29 @@
+package Maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class App {
+
+    public static void main(String[] args) {
+        // Key and value
+        // Key is unique
+        // Values can be duplicate
+        // Always access the value using its key
+
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "one");
+        map.put(2, "two");
+        map.put(3, "three");
+        map.put(4, "four");
+        map.put(5, "four");
+        map.put(null, "nullvalue");
+        map.remove(5);
+        System.out.println(map.get(2));
+        map.keySet();
+
+        System.out.println(map.toString());
+
+    }
+
+}
